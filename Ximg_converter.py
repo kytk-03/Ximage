1f9d76e1bede99787b1b7b1c6bd00f9e914f4fa7
Kiyotaka <kytk.khhut.328@gmail.com>
2019-11-11 16:34:48 +0900

    first commit

---
+++ Ximg_converter.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+
+import fabio
+import numpy as np
+import os
+from PIL import Image
+
+class Ximg_converter:
+    PNG_MAX = 255
+    def __init__(self, path_to_img):
+        self.root = os.path.splitext(path_to_img)[0]
+        self.img = fabio.open(path_to_img)
+        self.pixel = self.img.data
+
+    def to_csv(self):
+        np.savetxt(self.root + ".csv", self.pixel)
+
+    def to_png(self):
+        image = Image.fromarray(self.PNG_MAX * (1 - self.pixel/self.pixel.max()))
+        image.convert("RGB").save(self.root + ".png")
